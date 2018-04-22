0a33ea49e0d3d986bdedde3cbcbbc66e4113377c
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-04-22 19:39:25 -0400

    Added a title screen

---
+++ Assets/Scripts/TitleContinue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleContinue : MonoBehaviour
+{
+
+	void Start()
+	{
+	}
+	
+	void Update()
+	{
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Level1");
+        }
+	}
+}
