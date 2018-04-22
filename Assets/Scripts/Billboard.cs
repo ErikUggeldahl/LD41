14788c3147aeb4deaeef3fa1d83aa66b66c83187
Erik Uggeldahl <erikuggeldahl@gmail.com>
2018-04-22 10:34:07 -0400

    Added enemy health with health bar

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,11 @@
+﻿// http://answers.unity.com/answers/52690/view.html
+
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{	
+	void Update()
+	{
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    Transform healthbar;
+
+    const int TOTAL_HEALTH = 100;
+    int health = TOTAL_HEALTH;
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthbar.localScale = new Vector3((float)health / TOTAL_HEALTH, 1f, 1f);
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
